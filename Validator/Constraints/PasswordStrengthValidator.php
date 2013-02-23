002ad8c1fbddac59f01d29c0303805265dcbf1a4
John Bafford <john@bafford.com>
2013-02-22 21:39:19 -0500

    The validator doesn't need @Annotation

--- Validator/Constraints/PasswordStrengthValidator.php
+++ Validator/Constraints/PasswordStrengthValidator.php
@@ -5,9 +5,6 @@ namespace Bafford\PasswordStrengthBundle\Validator\Constraints;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
-/**
- * @Annotation
- */
 class PasswordStrengthValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
