480b2d34eac92eceaa4326df1d1add54904110a1
Denis Ah-Kang <denis@w3.org>
2013-10-02 19:12:50 +0400

    Update error messages

--- Validator/Constraints/PasswordStrength.php
+++ Validator/Constraints/PasswordStrength.php
@@ -13,8 +13,8 @@ class PasswordStrength extends Constraint
     public $tooLongMessage = 'Your password must not exceed {{length}} characters.';
     public $missingLettersMessage = 'Your password must include at least one letter.';
     public $requireCaseDiffMessage = 'Your password must include both upper and lower case letters.';
-    public $missingNumbersMessage = 'Your password must include at least one number.';
-    public $missingNonAlphanumericMessage = 'Your password must include at least one non alphanumeric character.';
+    public $missingNumbersMessage = 'Your password must include at least one digit.';
+    public $missingNonAlphanumericMessage = 'Your password must include at least one non-alphanumeric character.';
     
     public $minLength = 5;
     public $maxLength = 0;
